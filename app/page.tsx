9bf7c61c9b6df06dfcdace69afd7ec9a092f27fc
FraGa <francesco.gatti.dev@gmail.com>
2023-08-19 17:15:20 +0200

    [3] Render users: - Add "use client" directive to make use of client loading - Add User and UserPagination interfaces - Add users and pagination state - Add getUsersData and call it on component load with useEffect - Render user cards using a basic tailwind style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,56 @@
+"use client"
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+interface UserPagination {
+  page: number;
+  total_pages: number;
+}
+
 export default function Home() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [pagination, setPagination] = useState<UserPagination>();
+
+  const getUsersData = async (page = 1) => {
+    const res = await (await fetch(`https://reqres.in/api/users?page=${page}`)).json();
+
+    if (!res) return console.log('Error fetching data');
+
+    setUsers(res.data);
+    setPagination({
+      page: res.page,
+      total_pages: res.total_pages,
+    });
+  };
+
+  useEffect(() => {
+    getUsersData();
+  }, []);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>
         This is a test app to try out the new features of Next.js 13.
       </p>
+      {users && users.map((user) => (
+        // create a tailwind user card for each user
+        <div key={user.first_name} className="flex flex-col items-center justify-center rounded-lg shadow-lg bg-gray-100 p-4 m-4">
+          <Image width={300} height={300} className="w-32 h-32 rounded-full" src={user.avatar} alt={user.first_name} />
+          <div className="text-center">
+            <h2 className="text-lg font-semibold">{user.first_name} {user.last_name}</h2>
+            <p className="text-gray-600">{user.email}</p>
+          </div>
+        </div>
+      ))}
     </main>
   )
 }
