ad0e63387f56ea3cb770a4c1c32a9adea2c236bf
FraGa <francesco.gatti.dev@gmail.com>
2023-08-20 12:27:08 +0200

    [7] Style user list - Add tailwind style for user list - Add "no more users" at the end

--- app/page.tsx
+++ app/page.tsx
@@ -85,21 +85,19 @@ export default function Home() {
   );
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        This is a test app to try out the new features of Next.js 13.
-      </p>
-      {users && users.map((user) => (
-        // create a tailwind user card for each user
-        <div key={user.id} className="flex flex-col items-center justify-center rounded-lg shadow-lg bg-gray-100 p-4 m-4">
-          <Image width={300} height={300} className="w-32 h-32 rounded-full" src={user.avatar} alt={user.first_name} />
-          <div className="text-center">
-            <h2 className="text-lg font-semibold">{user.first_name} {user.last_name}</h2>
-            <p className="text-gray-600">{user.email}</p>
-          </div>
-        </div>
-      ))}
-      <div ref={observerTarget}></div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
+      <div className="container mx-auto p-4">
+        <ul className="divide-y">
+          {users && users.map((user) => (
+            <li key={user.id} className="flex items-center py-3">
+              <Image width={1000} height={1000} src={user.avatar} alt="User Avatar" className="w-20 h-20 md:w-40 md:h-40 rounded-full border border-gray-300" />
+              <span className="ml-4 md:ml-8 text-lg md:text-xl">{user.first_name} {user.last_name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {users && <div ref={observerTarget}></div>}
+      {pagination?.page === pagination?.total_pages && <p className="text-gray-600">No more users</p>}
     </main>
   )
 }
